ec24fa47a4e4ddd1e7e44fe8c7b761848d3f301e
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-03-31 13:48:13 -0400

    initial commit

---
+++ contactManagement/Data/ContactInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace contactManagement.Data
+{
+    public class ContactInfo
+    {
+        public Guid Id { get; set; }
+    }
+}
