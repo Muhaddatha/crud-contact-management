dd90841dfad41489a485b001117d31438426355b
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-03-31 14:13:50 -0400

    Added crud operations

--- contactManagement/Data/ContactService.cs
+++ contactManagement/Data/ContactService.cs
@@ -14,6 +14,39 @@ namespace contactManagement.Data
             _db = db;
         }
 
+        //get all employees 
+        public List<ContactInfo> GetContacts()
+        {
+            var contactList = _db.Contacts.ToList();
+            return contactList;
+        }
+
+        public string Create(ContactInfo contactInfo)
+        {
+            _db.Contacts.Add(contactInfo);
+            _db.SaveChanges();
+            return "Saved sucessfully";
+        }
+
+        public ContactInfo GetContactById(Guid Id)
+        {
+            ContactInfo contact = _db.Contacts.FirstOrDefault(s => s.Id == Id);
+            return contact;
+        }
+
+        public string UpdateContact(ContactInfo contact)
+        {
+            _db.Contacts.Update(contact);
+            _db.SaveChanges();
+            return "Updated successfully";
+        }
+
+        public string DeleteContactInfo(ContactInfo contact)
+        {
+            _db.Remove(contact);
+            _db.SaveChanges();
+            return "Deleted successfully";
+        }
 
     }
 }
