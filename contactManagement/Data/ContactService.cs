091acc64ddf1438fd2b7d31090e269e044923371
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-03-31 14:07:17 -0400

    connected to database

---
+++ contactManagement/Data/ContactService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace contactManagement.Data
+{
+    public class ContactService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ContactService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+    }
+}
--- contactManagement/Startup.cs
+++ contactManagement/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +30,7 @@ namespace contactManagement
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
