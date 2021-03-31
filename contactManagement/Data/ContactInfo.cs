8f00ae9d47dd53228edd100adeb2e73f5d38c273
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-03-31 13:50:44 -0400

    created contact object

--- contactManagement/Data/ContactInfo.cs
+++ contactManagement/Data/ContactInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,17 @@ namespace contactManagement.Data
 {
     public class ContactInfo
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime BirthDate { get; set; }
     }
 }
